88af890a43cb0b45fa425cfb18df7caaa6e33843
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-02-15 11:41:35 -0500

    now works with sparse matrices

--- bernoulli_NB.py
+++ bernoulli_NB.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import log, e, inf
 from sklearn.naive_bayes import BernoulliNB
 from collections import Counter
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csc_matrix
 from tqdm import trange
 """
 @SC for shame/blame
@@ -20,13 +20,13 @@ class homemade_BernoulliNB():
         self.learned_logprob_feature_given_class = None 
         # should there be a "predictions" attribute?
     
-    def fit( self, binary_feature_array: np.ndarray, binary_class_vec: np.ndarray, laplace_smoothing=True, verbose=True ):
+    def fit( self, binary_feature_array, binary_class_vec: np.ndarray, laplace_smoothing=True, verbose=True ):
         """
         Computes the MLEs P(feature_i == 1 | target == 1) and the class probabilities in a 2-class (binary classification) context. 
 
         Arguments:
 
-            binary_feature_array: a | instances | x | features | numpy array.
+            binary_feature_array: a | instances | x | features | numpy array or csr_matrix (gets converted to a binary csr_matrix either way).
 
             binary_class_vec: | instances |-sized binary numpy vector.
 
@@ -47,17 +47,21 @@ class homemade_BernoulliNB():
         """
 
         # some sanity checks
-        if isinstance( binary_feature_array, csr_matrix ): 
-            # might be faster to do binary_feature_array = binary_feature_array.toarray()
-            temp = binary_feature_array.toarray()
-            del binary_feature_array
-            binary_feature_array = temp
-
-        if binary_feature_array.dtype != 'bool':
-            binary_feature_array = binary_feature_array.astype( bool )
         
+        if isinstance( binary_feature_array, csr_matrix ): 
+            bool_feature_array = binary_feature_array.astype( bool ).astype( int )
+            del binary_feature_array # for memory footprint
+            binary_feature_array = bool_feature_array.transpose() # converts the features from column vectors to row vectors (in order to do dot product for counts (faster))
+            del bool_feature_array
+
+        if isinstance( binary_feature_array, np.ndarray ):
+            bool_feature_array = binary_feature_array.astype( bool ).astype( int )
+            del binary_feature_array # for memory footprint
+            binary_feature_array = csr_matrix( bool_feature_array.T ) # converts the features from column vectors to row vectors (in order to do dot product for counts (faster))
+            del bool_feature_array 
+
         if binary_class_vec.dtype != 'bool':
-            binary_class_vec = binary_class_vec.astype( bool )
+            binary_class_vec = binary_class_vec.astype( bool ).astype( int )
 
         ## if targets is a (1,#) or (#,1) array instead of a (#,) vector
         if binary_class_vec.ndim > 1:
@@ -68,27 +72,45 @@ class homemade_BernoulliNB():
         count_target0 = len( binary_class_vec ) - count_target1
 
         # compute P(feature_i | target=1)
-        ## dummy initializations
-        prob_feat_i_cond_1 = np.array( [ 0.0 ] * binary_feature_array.shape[1] , dtype=np.float64 )
-        prob_feat_i_cond_0 = np.array( [ 0.0 ] * binary_feature_array.shape[1] , dtype=np.float64 )
+        ## dummy initializations -- no longer needed with dot product shortcut
+        # prob_feat_i_cond_1 = np.array( [ 0.0 ] * binary_feature_array.shape[0] , dtype=np.float64 ) # remember that the binary_feature_array has been transposed!
+        # prob_feat_i_cond_0 = np.array( [ 0.0 ] * binary_feature_array.shape[0] , dtype=np.float64 ) # remember that the binary_feature_array has been transposed!
 
         ## used for P( y=0 | x ), swaps Trues -> Falses
-        inverted_binary_class_vec = np.invert( binary_class_vec ) 
+        inverted_binary_class_vec = 1.0 - ( binary_class_vec ) 
         
         if verbose:
             print("starting the fitting/learning process")
-        for feat_i in trange( binary_feature_array.shape[1] ):
+
+        prob_feat_i_cond_1 = binary_feature_array.dot( binary_class_vec )
+        prob_feat_i_cond_0 = binary_feature_array.dot( inverted_binary_class_vec )
+        '''
+        for feat_i in trange( binary_feature_array.shape[0] ): # remember that the binary_feature_array has been transposed!
+            
+            feature_column = binary_feature_array.getrow( feat_i )
+
+            
+            # Old code for dense arrays, can lead to memory errors 
+            prob_feat_i_cond_1[ feat_i ] =  np.sum( 
+                                                np.logical_and( feature_column, binary_class_vec ) 
+                                            )
+            prob_feat_i_cond_0[ feat_i ] =  np.sum( 
+                                                np.logical_and( feature_column, inverted_binary_class_vec ) 
+                                            )
+            Note: 
             # np.logical_and( np.array( [ True, True, False, False ] ), 
             #                 np.array( [ True, False, True, False ] ) )
             # yields True False False False
+            
 
-            prob_feat_i_cond_1[ feat_i ] =  np.sum( 
-                                                np.logical_and( binary_feature_array[:,feat_i], binary_class_vec ) 
-                                            )
-            prob_feat_i_cond_0[ feat_i ] =  np.sum( 
-                                                np.logical_and( binary_feature_array[:,feat_i], inverted_binary_class_vec ) 
-                                            )
+            #class1res = feature_column.dot( binary_class_vec )
+            #assert class1res.shape == (1,)
+            #prob_feat_i_cond_1[ feat_i ] = class1res[0]
 
+            #class0res = feature_column.dot( inverted_binary_class_vec )
+            #assert class0res.shape == (1,)
+            #prob_feat_i_cond_0[ feat_i ] = class0res[0]
+        '''
         if laplace_smoothing: 
             prob_feat_i_cond_1 = ( prob_feat_i_cond_1 + 1 ) / ( count_target1 + 2 )
             prob_feat_i_cond_0 = ( prob_feat_i_cond_0 + 1 ) / ( count_target0 + 2 )
@@ -102,13 +124,13 @@ class homemade_BernoulliNB():
         self.class_log_probs[0] = log( count_target0 / len( binary_class_vec ) )
         self.class_log_probs[1] = log( count_target1 / len( binary_class_vec ) )
 
-    def predict( self, test_instances: np.ndarray, verbose=True ):
+    def predict( self, test_instances, verbose=True ):
         """
         Returns predicted class for test_instance using MLE/MAP parameters and class probabilities.
 
         Arguments:
 
-            test_instances: | testing instances | x | features |-sized numpy array.
+            test_instances: | testing instances | x | features |-sized numpy array or csr_matrix (get converted to a csr_matrix either way).
 
             verbose: boolean indicator of verbosity.
         
@@ -121,10 +143,14 @@ class homemade_BernoulliNB():
 
         # sanity checks
         if isinstance( test_instances, csr_matrix ): 
-            # might be faster to do binary_feature_array = binary_feature_array.toarray()
-            temp = test_instances.toarray()
-            del test_instances
-            test_instances = temp
+            bool_feature_array = test_instances.astype( bool ).astype( int ) # ensures a <float > 0 > -> 1 mapping
+            del test_instances # for memory footprint
+            test_instances = bool_feature_array
+
+        if isinstance( test_instances, np.ndarray ):
+            bool_feature_array = test_instances.astype( bool ).astype( int )
+            del test_instances # for memory footprint
+            test_instances = csr_matrix( bool_feature_array )
 
         # dummy initializations
         class_preds, ml_prob = np.array( [None]*test_instances.shape[0] ), np.array( [-inf]*test_instances.shape[0] )
@@ -132,8 +158,10 @@ class homemade_BernoulliNB():
         # loop over instances 
         if verbose:
             print("starting to make predictions")
+            
         for test_instance_index in trange( test_instances.shape[0] ):
-            test_instance = test_instances[ test_instance_index, : ]
+            test_instance_csr = test_instances[ test_instance_index, : ]
+            test_instance = test_instance_csr.toarray().flatten()
         # for test_instance_index, test_instance in enumerate( test_instances ):
             # iterate over every class, keeping track of which class has the highest probability
             for row_index, row_class_logprobs in enumerate( self.learned_logprob_feature_given_class ):
@@ -244,10 +272,10 @@ if __name__ == '__main__':
 
     # random initialization of a training dataset
     classes =  np.random.randint( 0, 2, ( 100 ) )
-    features = np.random.randint( 0, 2, ( 100, 100 ) )
+    features = csr_matrix( np.random.randint( 0, 2, ( 100, 30 ) ) )
 
     # using the functions individually, res == learned probabilities from training
-    logp0, logp1, res =  binary_bernoulli_NB( features, classes) 
+    # logp0, logp1, res =  binary_bernoulli_NB( features, classes) 
     
     # sanity check with sklearn's BernoulliNB classifier
     sanity_check = BernoulliNB()
@@ -256,20 +284,21 @@ if __name__ == '__main__':
     # sanity check with homemade_BernoulliNB class
     homemade_sanity_check = homemade_BernoulliNB()
     homemade_sanity_check.fit( features, classes )
-
+    
     # generating 1,000,000 random test datasets
     for i in range( 100000 ):
         print( i )
-        tests = np.random.randint( 0, 2, ( 10, 100 ) )
+        tests = np.random.randint( 0, 2, ( 10, 30 ) )
         
         sanity_preds = sanity_check.predict( tests )
         homemade_sanity_preds = homemade_sanity_check.predict( tests )
         
-        my_preds = []
+        '''my_preds = []
         for t in tests:
             my_preds.append( predict( res, [ logp0, logp1 ], t ) )
+        '''
         
-        assert ( np.array_equal( np.array( my_preds ), sanity_preds ) )
+        #assert ( np.array_equal( np.array( my_preds ), sanity_preds ) )
         
         assert( np.array_equal( sanity_preds, homemade_sanity_preds ) )
     
