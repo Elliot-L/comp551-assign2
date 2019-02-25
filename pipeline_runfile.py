7bd1665d3ccf208a07a78721b53d2bcf6311c266
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-02-25 15:12:05 -0500

    some minor edits and changes

--- pipeline_runfile.py
+++ pipeline_runfile.py
@@ -1,6 +1,7 @@
 # standard lib dependencies
 import os, operator, random, json, csv, re, pickle
 import numpy as np
+import pandas as pds
 
 from tqdm import trange
 from collections import Counter
@@ -33,10 +34,90 @@ from sklearn.neighbors import KNeighborsClassifier
 # homemade dependencies
 from bernoulli_NB import *
 from pipelines import strat_k_fold
-from tf_idf import fetch_instances, CustomTokenizer, loader, word_tokenize
+from tf_idf import *
 
 # external dependencies
 from nbsvm import NBSVM
+
+def make_testing_feature_matrix( path_to_dir, vocabulary_kwarg=None ):
+
+    if path_to_dir is None:
+        path_to_dir=os.path.join( '..','comp-551-imbd-sentiment-classification', 'test' )
+
+    testing_instances = []
+    testing_instances_names = []
+    for file in [ os.path.join( path_to_dir, f ) for f in os.listdir( path_to_dir )  ]:
+        with open( file, encoding="utf8" ) as f:
+            testing_instances_names.append( str( os.path.basename( file ) ) )
+            testing_instances.append( f.read() )
+
+    word_count = np.array(
+            [ len( word_tokenize( instance ) ) for instance in testing_instances ]
+        )
+    
+    sentences_count = np.array( 
+            [ len( sentence_tokenize( instance, ntop=0 ) ) for instance in testing_instances ]
+        )
+    
+    avg_word_per_sentences = word_count / sentences_count
+
+    # valence stuff
+    wea_valence_file_path = "Ratings_Warriner_et_al-1.csv"
+    valence_df = pds.read_csv( wea_valence_file_path, sep=',', usecols=[1,2], index_col=0 )
+    mean_valence = np.mean( valence_df["V.Mean.Sum"] )
+    valent_scores_list, valent_word_counts_list = valence_score_review( testing_instances, valence_df, mean_valence, flip_if_not_in_ngram=1 )
+    min_valence = abs( min( valent_scores_list ) )
+    for i,v in enumerate( valent_scores_list ): # nbsvm assumes the input X is all positive
+        valent_scores_list[i] += min_valence
+
+    # converting to csr_matrices
+    word_count = csr_matrix( word_count ).transpose()
+    sentences_count = csr_matrix( sentences_count ).transpose()
+    avg_word_per_sentences = csr_matrix( avg_word_per_sentences ).transpose()
+    valent_scores = csr_matrix( valent_scores_list ).transpose()
+    valent_word_counts = csr_matrix( valent_word_counts_list ).transpose()
+    valency_ratio_list = []
+    for ( vs,vwc ) in zip( valent_scores_list, valent_word_counts_list ):
+        if vwc == 0:
+            valency_ratio_list.append(0.0)
+        else:
+            valency_ratio_list.append( vs / vwc )
+    valency_ratio = csr_matrix( 
+        valency_ratio_list
+    ).transpose()
+    print(f"shape of valency_ratio_list = {valency_ratio.shape}")
+    # not used
+    ##positive_word_overlap_csr = csr_matrix(
+    ##    np.array( positive_word_overlap )
+    ##).transpose()
+    ##negative_word_overlap_csr = csr_matrix(
+    #3    np.array( negative_word_overlap )
+    #3).transpose()
+
+    testing_count_feat_mat, testing_count_vectorizer = create_count_matrix( testing_instances, vocabulary_kwarg=vocabulary_kwarg )
+    token_to_col_index_dict = testing_count_vectorizer.vocabulary_ 
+
+    # training_tfidf_feat_mat, training_tfidf_vectorizer = create_tfidf_matrix( pos_instances_list+neg_instances_list, vocabulary_kwarg=token_to_col_index_dict )
+
+
+    ### Add features here ###
+    testing_count_feat_mat = csr_matrix(
+        hstack(
+            [ testing_count_feat_mat, 
+            word_count, 
+            sentences_count, 
+            avg_word_per_sentences,
+            valent_scores,
+            valent_word_counts,
+            valency_ratio#,
+            #positive_word_overlap_csr,
+            #negative_word_overlap_csr 
+            ]
+        )
+    )
+
+    return testing_count_feat_mat, testing_instances_names
+
 
 def iris_logreg():
 
@@ -102,21 +183,58 @@ def run_pipelines( verbose=True ):
     pipelines = [ 
         bernoulli_Pipeline, 
         logreg_pipeline, 
-        rf_pipeline, 
+        #  rf_pipeline, # not accurate
         linscv_pipeline, 
         multinomial_Pipeline, 
         nbsvm_Pipeline, 
-        knn_Pipeline
-        #gbclf_Pipeline 
+        # knn_Pipeline # not accurate
+        #gbclf_Pipeline # slow af
     ]
 
     # X, y, vectorizer = loader( "unigram_homebrew_tokenized_count_feat_mat_labels_and_vectorizer.pickle", pickled_file=True )
-    with open( 'entire_(1-2)_wc_sc_wps_homebrew_tfidf_feat_mat_labels_and_vectorizer.pickle', 'rb' ) as handle:
+    with open( 'entire_wea-valency_(1-2)_wc_sc_wps_homebrew_count_feat_mat_labels_and_vectorizer.pickle', 'rb' ) as handle:
         try:
             X, y, vectorizer, mapping, metadata = pickle.load( handle )
         except ValueError:
             X, y, vectorizer = pickle.load( handle )
 
+    #trunc_SVD = TruncatedSVD( n_components=50 )
+    #trunc_SVD.fit( X )
+
+    
+
+    '''
+    for training on the whole dataset.
+    '''
+    
+    print("starting training")
+    nbsvm_Pipeline.fit( X, y )
+
+    print("making test feature matrix")
+    testing_feature_matrix, testing_filenames = make_testing_feature_matrix( 
+        os.path.join( '..','comp-551-imbd-sentiment-classification', 'test' ), vocabulary_kwarg=vectorizer.vocabulary_  
+    )
+    print("making predictions")
+    predictions = nbsvm_Pipeline.predict( testing_feature_matrix )
+
+    for ( pred, file ) in zip( predictions, testing_filenames ):
+        print( f"{file}\t{pred}")
+    
+    file_numbers = [ int( name.split('.txt')[0] ) for name in testing_filenames ]
+    results_np = np.zeros( ( len( testing_filenames ), 2 ) )
+    for index, ( pred, file ) in enumerate( zip( predictions, file_numbers ) ):
+        results_np[ index, 0 ] = file
+        results_np[ index, 1 ] = pred
+
+    results_df = pds.DataFrame( results_np, columns=["Id", "Category"] )
+    results_df.sort_values(by=['Id'])
+    results_df.to_csv( "results.tsv", sep='\t', index=False, header=True, mode='w' )
+    
+    with open("output.txt", "w") as outfile:
+        outfile.write("Id   Category\n")
+        for ( pred, file ) in zip( predictions, testing_filenames ):
+            outfile.write( f"{file}\t{pred}\n") 
+    '''
     splits = strat_k_fold(X, y, k=5)
     num_sets = len(splits[0])
     for i in range(num_sets):
@@ -131,16 +249,7 @@ def run_pipelines( verbose=True ):
             if verbose:
                 print( f"training { list( pipeline.named_steps.keys() )[0]}")
             pipeline.fit( train_x, train_y )
-        '''
-        bernoulli_Pipeline.fit( train_x, train_y )
-        logreg_pipeline.fit( train_x, train_y )
-        rf_pipeline.fit( train_x, train_y )
-        linscv_pipeline.fit( train_x, train_y )
-        multinomial_Pipeline.fit( train_x, train_y )
-        nbsvm_Pipeline.fit( train_x, train_y )
-        gbclf_Pipeline.fit( train_x, train_y )
-        '''
-
+        
         predictions = {}
         for pipeline in pipelines:
             if verbose:
@@ -151,25 +260,8 @@ def run_pipelines( verbose=True ):
         for pipeline_name, predictions in predictions.items():
             print( f"{pipeline_name}: {accuracy_score( valid_y, predictions ) }")
             
-        
-        '''bernoulli_NB_preds = bernoulli_Pipeline.predict( valid_x )
-        logreg_preds = logreg_pipeline.predict( valid_x )
-        rf_preds = rf_pipeline.predict( valid_x )
-        linscv_preds = linscv_pipeline.predict( valid_x )
-        multinomi_NB_preds = multinomial_Pipeline.predict( valid_x )
-        nbsvm_preds = nbsvm_Pipeline.predict( valid_x )
-        #gbclf_preds = gbclf_Pipeline.predict( valid_x )
-        
-        
-        print('===')
-        print( f"bernoulliNB: {accuracy_score( valid_y, bernoulli_NB_preds )}" )
-        print( f"logreg: {accuracy_score( valid_y, logreg_preds )}" )
-        print( f"randomforrest: {accuracy_score( valid_y, rf_preds )}" )
-        print( f"linscv: {accuracy_score( valid_y, linscv_preds )}")
-        print( f"multinomialNB: { accuracy_score( valid_y, multinomi_NB_preds )}")
-        print( f"nbsvm: { accuracy_score( valid_y, nbsvm_preds )}")
-        print( f"gbclf: { accuracy_score( valid_y, gbclf_preds )}")'''
-    
+    '''
+
     print("done")
 
 if __name__ == '__main__':
